29f6fbf660a247fa737139921048579280d21e2e
ronitpadariya <rntpadariya38@gmail.com>
2026-05-02 18:49:33 +0530

    Store the funder's addresses with values

--- FundMe.sol
+++ FundMe.sol
@@ -7,12 +7,17 @@ contract FundMe {
 
     uint256 public minimumUsd = 5e18;
 
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+
     function fund() public payable {
         // Allow users to send $
         // Have a minimum send 5$
         // 1. How do we send ETH to this contract?
         require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough ETH");  // 1e18 = 1 ETH = 1000000000000000000
         // https://api
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
 
     }
 
