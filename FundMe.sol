c5168682e7e73527fa90679230905b23c8a4085a
ronitpadariya <rntpadariya38@gmail.com>
2026-05-05 19:08:03 +0530

    Reset funder array. Test transfer and send ETH. Implement Call function

--- FundMe.sol
+++ FundMe.sol
@@ -17,8 +17,8 @@ contract FundMe {
         
         require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough ETH");  // 1e18 = 1 ETH = 1000000000000000000
         
-        addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
 
     }
 
@@ -27,22 +27,30 @@ contract FundMe {
         return priceFeed.version();
     }
 
-    // function withdraw() public {}
+    function withdraw() public {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
 
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        // 1 ETH ?
-        // 2000_000000000000000000
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
+        // reset array
+        funders = new address[](0);
+
+        // transfer
+        // (bool success, ) = payable(msg.sender).transfer(address(this).balance);
+        // (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        // require(success, "Transfer failed");
+
+        // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // (bool sendSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        // require(sendSuccess, "Send failed");
+        
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
 
-    function getPrice() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        // price of ETH in terms of USD;
-        // 2000.00000000
-        return uint256(price * 1e10);
-    }
+    
 
 }
